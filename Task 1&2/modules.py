bae9677be6d8b114bbae381be41fc5aa9cbd15d8
AndreDale368 <oo2086@bristol.ac.uk>
2023-04-25 14:24:53 +0100

    Added Docstring to wavefront function

--- Task 1&2/modules.py
+++ Task 1&2/modules.py
@@ -340,5 +340,18 @@ def solve(grid, n_rows, n_cols):
 	return grid, filled_in, iterations
 
 def wavefront(grid, n_rows, n_cols, row, column):
-	
+	"""
+	Parameters:
+	--------------
+	grid: list
+		A list of lists representing a sudoku board
+	n_rows: int
+		The number of rows in each square
+	n_cols: int
+		The number of columns in each square
+	row: int
+		The row index of the cell to be filled
+	column: int
+		The column index of the cell to be filled
+	"""
 	pass
