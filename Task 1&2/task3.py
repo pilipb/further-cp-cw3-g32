92621e35f3cfe68cfe69130020747c6db199fd7c
AndreDale368 <oo2086@bristol.ac.uk>
2023-04-25 14:35:13 +0100

    CREATE task3.py

---
+++ Task 1&2/task3.py
@@ -0,0 +1,21 @@
+from possible_values import possible_values_combined
+
+def wavefront(grid, n_rows, n_cols, row, column):
+	"""
+	Parameters:
+	--------------
+	grid: list
+		A list of lists representing a sudoku board
+	n_rows: int
+		The number of rows in each square
+	n_cols: int
+		The number of columns in each square
+	row: int
+		The row index of the cell to be filled
+	column: int
+		The column index of the cell to be filled
+	"""
+	grid_copy = grid
+	n = n_rows * n_cols
+
+	pass
