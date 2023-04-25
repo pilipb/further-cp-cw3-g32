7eb779abd76bd80a08f7d67d67adbcb1ec21b1b6
AndreDale368 <oo2086@bristol.ac.uk>
2023-04-25 14:35:55 +0100

    MOVED wavefront function to task3.py

--- Task 1&2/modules.py
+++ Task 1&2/modules.py
@@ -338,23 +338,3 @@ def solve(grid, n_rows, n_cols):
 	filled_in = find_filled(original_grid, grid)
 
 	return grid, filled_in, iterations
-
-def wavefront(grid, n_rows, n_cols, row, column):
-	"""
-	Parameters:
-	--------------
-	grid: list
-		A list of lists representing a sudoku board
-	n_rows: int
-		The number of rows in each square
-	n_cols: int
-		The number of columns in each square
-	row: int
-		The row index of the cell to be filled
-	column: int
-		The column index of the cell to be filled
-	"""
-	grid_copy = grid
-	n = n_rows * n_cols
-
-	pass
