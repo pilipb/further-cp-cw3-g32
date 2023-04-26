8ef8882a09f198243426f445fbc66b9dccc14a64
livbrannan <gr21885@bristol.ac.uk>
2023-04-26 15:30:06 +0100

    Adding comments

--- Task 1&2/task3.py
+++ Task 1&2/task3.py
@@ -17,13 +17,15 @@ def possible_values_grid(grid, n_rows, n_cols, row, column):
 	"""
 	grid_copy = grid 
 	n = n_rows * n_cols
-
+ 
+ #creating a list of lists of lists for the possible values in each square
 	for i in range(n): 
 		for j in range(n):
 			if grid_copy[i][j] == 0:
 				possible_values = list(possible_values_combined(grid_copy, n_rows, n_cols, i, j))
 				grid_copy[i][j] = possible_values
 	return grid_copy
+
 
 test_grid = [
 		[0, 0, 6, 0, 0, 3],
