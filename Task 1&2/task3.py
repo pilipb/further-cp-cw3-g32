8b0c926e1a94272d56d93f0e933f21b3f13eb0f1
AndreDale368 <oo2086@bristol.ac.uk>
2023-04-28 15:35:31 +0100

    ADD test_grid2 to task3.py

--- Task 1&2/task3.py
+++ Task 1&2/task3.py
@@ -35,4 +35,13 @@ test_grid = [
 		[0, 0, 1, 0, 0, 0],
 		[0, 5, 0, 0, 6, 4]]
 
-print(possible_values_grid(test_grid, 2, 3, 0, 0))
+#print(possible_values_grid(test_grid, 2, 3, 0, 0))
+
+test_grid2 = [
+		[1,2,3,4],
+		[3,4,1,2],
+		[2,3,4,1],
+		[4,1,2,0]
+]
+
+print(possible_values_grid(test_grid2, 2, 2, 0, 0))
