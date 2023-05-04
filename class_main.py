51c2073b306b593ed62bcf89e23981143b2cbbbd
pilipb <xq19351@bristol.ac.uk>
2023-05-04 09:30:13 +0100

    cleaner

--- class_main.py
+++ class_main.py
@@ -27,14 +27,21 @@ def main():
     # Check if the input is valid - only contains valid flags and arguments
     try:
         flag_dict, flag_value = input_collection(sys.argv[1:])
+
+    # If not, print the error and exit
     except ValueError as e:
         print("Error: ",e)
         sys.exit()
+
+    # If the file flag is enabled, run the file input function and exit
     if flag_dict['-file'] == True:
         # Run seperate file input function and exit
         file_input_main(flag_value['-file'][0], flag_value['-file'][1],  flag_dict['-hint'], flag_value['-hint'],flag_dict['-explain'], flag_dict['-profile'])
         return 
-    # If not, pull the grids from grids.py and intialise the Sudoku class for each grid and store in a dictionary
+    
+    ##### IF THERE IS NO FILE INPUT #####
+    # The grids from grids.py are run automatically
+
     # Initialise the grid storage dictionary to store the Sudoku class instances
     grid_storage = {}
     # Initialise the solve metrics dictionary to store the profiling metrics if profiling is enabled
@@ -50,12 +57,17 @@ def main():
                                                    hint_number = flag_value['-hint'], 
                                                    profile_flag= flag_dict['-profile'],
                                                    explain_flag= flag_dict['-explain'])
-    # Iterate through the grid storage dictionary and:
+        
+
+    # Iterate through the grid storage dictionary and solve each grid using the wavefront and recursion methods
     for instance in grid_storage:
+
         # Solve the grid
         grid_storage[instance].wavefront_solve()
         grid_storage[instance].recursion_solve()
         output_grid = grid_storage[instance].grid
+
+
         # If the hint flag is enabled, generate the hint grid and return it
         if flag_dict['-hint'] == True:
             grid_storage[instance].hint_class()
@@ -65,12 +77,15 @@ def main():
             if flag_dict['-explain'] == False:
                 print('Hint Grid')
                 print(f'({flag_value["-hint"]} hints requested)')
+    
         # Print the appropriate grid
         print(instance)
         print(np.array(output_grid))
+
         # If the explain flag is enabled, print the explanation of the solution (either hints or full solution)
         if flag_dict['-explain'] == True:
             grid_storage[instance].explain_class()
+
         # If the profile flag is enabled, store the profiling metrics in the solve metrics dictionary
         if flag_dict['-profile'] == True:
             solve_metrics[instance] = [(grid_storage[instance].n_rows, grid_storage[instance].n_cols), 
@@ -78,6 +93,7 @@ def main():
                                        grid_storage[instance].zero_counter, 
                                        grid_storage[instance].iterations,
                                        ]
+            
     # If the profile flag is enabled, print the profiling metrics
     if flag_dict['-profile'] == True:
         # Print the profiling metrics
